a64eccf3c34ff2c1f25d68654e2a3146b4215ac7
Miguel Luna <miguelmendesluna@gmail.com>
2025-02-08 12:02:18 -0300

    Ajustando instruções para signed

--- leitorExibidor/conversions.c
+++ leitorExibidor/conversions.c
@@ -1,23 +1,25 @@
 #include "conversions.h"
 
 void i2l(Frame * frame, Instruction Instruction) {
-  uint32_t number = remove_from_stack(frame);
+  int32_t number = uint32_to_int(remove_from_stack(frame));
   if (number > 0x7fffffff) {
     add_to_stack(frame, 0xffffffff);
   } else {
     add_to_stack(frame, 0);
   }
-  add_to_stack(frame, number);
+  uint32_t converted_number = int_to_uint32(number);
+  add_to_stack(frame, converted_number);
 }
 
 void i2f(Frame * frame, Instruction Instruction) {
-  float number = (float) remove_from_stack(frame);
-  uint32_t converted_number = float_to_uint32(number);
+  int32_t number = uint32_to_int(remove_from_stack(frame));
+  float f_number = (float) number;
+  uint32_t converted_number = float_to_uint32(f_number);
   add_to_stack(frame, converted_number);
 }
 
 void i2d(Frame * frame, Instruction Instruction) {
-  uint32_t number = remove_from_stack(frame);
+  int32_t number = uint32_to_int(remove_from_stack(frame));
   double converted_number = (double) number;
   int hi;
   int lo;
@@ -55,15 +57,16 @@ void l2d(Frame * frame, Instruction Instruction) {
 
 void f2i(Frame * frame, Instruction Instruction) {
   float number = uint32_to_float(remove_from_stack(frame));
-  uint32_t converted_number = (uint32_t) number;
+  uint32_t converted_number = int_to_uint32((int32_t) number);
   add_to_stack(frame, converted_number);
 }
 
 void f2l(Frame * frame, Instruction Instruction) {
   float number = uint32_to_float(remove_from_stack(frame));
-  uint64_t converted_number = (uint64_t) number;
-  uint32_t low = (uint32_t) converted_number;
-  uint32_t high = (uint32_t) (converted_number >> 32);
+  int64_t converted_number = (int64_t) number;
+  uint64_t u_converted_number = long_to_uint64(converted_number);
+  uint32_t low = (uint32_t) u_converted_number;
+  uint32_t high = (uint32_t) (u_converted_number >> 32);
   add_to_stack(frame, high);
   add_to_stack(frame, low);
 }
@@ -83,7 +86,8 @@ void d2i(Frame * frame, Instruction Instruction) {
   uint64_t high = (uint64_t) remove_from_stack(frame);
   uint64_t number = high << 32 | low;
   double converted_number = uint64_to_double(number);
-  uint32_t final_number = (uint32_t) converted_number;
+  int32_t i_number = (int32_t) converted_number;
+  uint32_t final_number = int_to_uint32(i_number);
   add_to_stack(frame, final_number);
 }
 
@@ -92,11 +96,12 @@ void d2l(Frame * frame, Instruction Instruction) {
   uint64_t high = (uint64_t) remove_from_stack(frame);
   uint64_t number = high << 32 | low;
   double converted_number = uint64_to_double(number);
-  uint64_t final_long_number = (uint64_t) converted_number; 
+  int64_t final_i_number = (int64_t) converted_number;
+  uint64_t final_long_number = long_to_uint64(final_i_number);
   uint32_t final_low = (uint32_t) final_long_number;
   uint32_t final_high = (uint32_t) (final_long_number >> 32);
-  add_to_stack(frame, final_low);
   add_to_stack(frame, final_high);
+  add_to_stack(frame, final_low);
 }
 
 void d2f(Frame * frame, Instruction Instruction) {
@@ -110,20 +115,25 @@ void d2f(Frame * frame, Instruction Instruction) {
 }
 
 void i2b(Frame * frame, Instruction Instruction) {
-  uint8_t number = (uint8_t) remove_from_stack(frame);
-  uint32_t converted_number = (uint32_t) number;
+  int32_t number = uint32_to_int(remove_from_stack(frame));
+  int8_t b_number = (int8_t) number;
+  int32_t i_number = (int32_t) b_number;
+  uint32_t converted_number = int_to_uint32(i_number);
   add_to_stack(frame, converted_number);
 }
 
 void i2s(Frame * frame, Instruction Instruction) {
-  uint16_t number = (uint16_t) remove_from_stack(frame);
-  uint32_t converted_number = (uint32_t) number;
-  add_to_stack(frame, converted_number);
+  int32_t number = uint32_to_int(remove_from_stack(frame));
+  int16_t converted_number = (int16_t) (number &= 0x0000FFFF);
+  int32_t i_converted_number = (int32_t) converted_number;
+  uint32_t u_converted_number = int_to_uint32(i_converted_number);
+  add_to_stack(frame, u_converted_number);
 }
 
 void i2c(Frame * frame, Instruction Instruction) {
-  char number = (char) remove_from_stack(frame);
-  uint32_t converted_number = (uint32_t) number;
+  int32_t number = uint32_to_int(remove_from_stack(frame));
+  char c_number = (char) number;
+  uint32_t converted_number = (uint32_t) c_number;;
   add_to_stack(frame, converted_number);
 }
 
